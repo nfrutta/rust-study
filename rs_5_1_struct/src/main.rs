d225f92c9053da2a5f31b9755fe16c0322f889dd
naoyuki <nfrutta1208@gmail.com>
2021-04-20 22:39:44 +0900

    警告がうざいので一部コメントアウト

--- rs_5_1_struct/src/main.rs
+++ rs_5_1_struct/src/main.rs
@@ -2,8 +2,8 @@
 struct User {
     username: String,
     email: String,
-    sign_in_count: u64,
-    active: bool,
+    //sign_in_count: u64,
+    //active: bool,
 }
 
 fn main() {
@@ -11,26 +11,27 @@ fn main() {
     let user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
+        //active: true,
+        //sign_in_count: 1,
     };
+    println!("{}:{}", user1.username, user1.email);
 
     // こういうパターンもある
     let mut user2 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
+        //active: true,
+        //sign_in_count: 1,
     };
     user2.email = String::from("anotheremail@example.com");
+    println!("{}:{}", user2.username, user2.email);
 }
 
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        email, // 同じ名前なので省略できる
-        username, // ここも省略
-        active: true,
-        sign_in_count: 1,
-    }
-}
+// fn build_user(email: String, username: String) -> User {
+//     User {
+//         email, // 同じ名前なので省略できる
+//         username, // ここも省略
+//         active: true,
+//         sign_in_count: 1,
+//     }
+// }
