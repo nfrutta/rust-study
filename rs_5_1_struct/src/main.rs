d70522a6041ad6ceb5953f14dac9cc6460975c93
naoyuki <nfrutta1208@gmail.com>
2021-04-20 22:34:17 +0900

    構造体の基本

---
+++ rs_5_1_struct/src/main.rs
@@ -0,0 +1,36 @@
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn main() {
+    // 構造体のインスタンス生成
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    // こういうパターンもある
+    let mut user2 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    user2.email = String::from("anotheremail@example.com");
+}
+
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email, // 同じ名前なので省略できる
+        username, // ここも省略
+        active: true,
+        sign_in_count: 1,
+    }
+}
